57da6b9edf09de075714b8d1298b6f09f624953a
twinstop11-dotcom <twinstop11@gmail.com>
2026-07-19 16:17:50 +0900

    7.16숙제

---
+++ 7.16-7.py
@@ -0,0 +1,8 @@
+# i가 0부터 9까지
+for i in range(0,10):
+    print(10-i, end='.')
+
+# i가 10부터 1까지
+for i in range(10,0,-1):
+    print(i, end='.')
+
---
+++ 7.16.py
@@ -0,0 +1,11 @@
+a=int(input())
+sum=1
+i=1
+while i<a:
+    if a%i==0:
+        sum=sum+1
+    i=i+1
+print(sum)
+        
+    
+    
