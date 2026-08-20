0b0e74f8e91830146106cf4f8e07b91f73309af7
twinstop11-dotcom <twinstop11@gmail.com>
2026-08-20 16:59:17 +0900

    숙제

--- 8.16-6.py
+++ 8.16-6.py
@@ -1,9 +1,25 @@
+'''
 L=[]
+sum=0
 
 while True:
     a=input()
     if a=='끝':
         break
-    L.append()
-r=L.pop()
-print(r)
+    L.append(a)
+    sum=sum+1
+
+for i in range(sum):
+    print(L.pop(),end=' ')
+    '''
+
+L=[]
+
+a=input()
+while a!='끝':
+    L.append(a)
+    a=input()
+
+
+for i in range(len(L)):
+    print(L.pop(), end=' ')
---
+++ 8.20-2.py
@@ -0,0 +1,7 @@
+L=[]
+
+for i in range(5):
+    a=int(input())
+    L.append(a)
+L.sort()
+print(L[0],L[4])
